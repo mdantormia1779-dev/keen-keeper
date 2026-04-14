e5ec86093efe783212c8a6c44c1c9ca1018ddd78
Md Antor Mia <your-email@gmail.com>
2026-04-15 02:25:51 +0600

    fotter design done

--- src/app/components/Card/Card.jsx
+++ src/app/components/Card/Card.jsx
@@ -1,73 +1,25 @@
 import Image from 'next/image';
 import React from 'react';
-import Images from "../../../../public/ellipse-1.png"
 
 
-const Card = () => {
+const Card = ({ friend }) => {
     return (
-        <div className='container mx-auto p-4 lg:p-0'>
-            <h1 className='font-bold text-[#1f2937] text-2xl'>Your Friends</h1>
-            <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-                <div className='bg-white p-8 flex flex-col justify-center items-center rounded-lg shadow-lg'>
-                    <Image className='mb-3'
-                        src={Images}
-                        width={80}
-                        height={80}
-                        alt="Picture of the author"
-                    />
-                    <h1 className='font-semibold text-xl text-[#1f2937] mb-2'>David Kim</h1>
-                    <p className='text-[#64748b]'>62d ago</p>
-                    <div className="badge badge-info font-semibold text-[#244d3f] bg-[#cbfadb] mt-2">WORK</div>
-                    <div className="badge badge-warning text-white bg-[#efad44] rounded-full mt-2">Warning</div>
+        <div>
+            <div className='bg-white p-8 flex flex-col justify-center items-center rounded-lg shadow-lg'>
+                <Image className='mb-3 rounded-full'
+                    src={friend.picture}
+                    width={80}
+                    height={80}
+                    alt="Picture of the author"
+                />
+                <h1 className='font-semibold text-xl text-[#1f2937] mb-2'>{friend.name}</h1>
+                <p className='text-[#64748b]'>{friend.days_since_contact}d ago</p>
+                <div>
+                    {friend.tags.map((tag,index) => (
+                        <div key={index} className="badge badge-info ml-4 font-semibold text-[#244d3f] bg-[#cbfadb] mt-2">{tag}</div>
+                    ))}
                 </div>
-                <div className='bg-white p-8 flex flex-col justify-center items-center rounded-lg shadow-lg'>
-                    <Image className='mb-3'
-                        src={Images}
-                        width={80}
-                        height={80}
-                        alt="Picture of the author"
-                    />
-                    <h1 className='font-semibold text-xl text-[#1f2937] mb-2'>David Kim</h1>
-                    <p className='text-[#64748b]'>62d ago</p>
-                    <div className="badge badge-info font-semibold text-[#244d3f] bg-[#cbfadb] mt-2">WORK</div>
-                    <div className="badge badge-warning text-white bg-[#efad44] rounded-full mt-2">Warning</div>
-                </div>
-                <div className='bg-white p-8 flex flex-col justify-center items-center rounded-lg shadow-lg'>
-                    <Image className='mb-3'
-                        src={Images}
-                        width={80}
-                        height={80}
-                        alt="Picture of the author"
-                    />
-                    <h1 className='font-semibold text-xl text-[#1f2937] mb-2'>David Kim</h1>
-                    <p className='text-[#64748b]'>62d ago</p>
-                    <div className="badge badge-info font-semibold text-[#244d3f] bg-[#cbfadb] mt-2">WORK</div>
-                    <div className="badge badge-warning text-white bg-[#efad44] rounded-full mt-2">Warning</div>
-                </div>
-                <div className='bg-white p-8 flex flex-col justify-center items-center rounded-lg shadow-lg'>
-                    <Image className='mb-3'
-                        src={Images}
-                        width={80}
-                        height={80}
-                        alt="Picture of the author"
-                    />
-                    <h1 className='font-semibold text-xl text-[#1f2937] mb-2'>David Kim</h1>
-                    <p className='text-[#64748b]'>62d ago</p>
-                    <div className="badge badge-info font-semibold text-[#244d3f] bg-[#cbfadb] mt-2">WORK</div>
-                    <div className="badge badge-warning text-white bg-[#efad44] rounded-full mt-2">Warning</div>
-                </div>
-                <div className='bg-white p-8 flex flex-col justify-center items-center rounded-lg shadow-lg'>
-                    <Image className='mb-3'
-                        src={Images}
-                        width={80}
-                        height={80}
-                        alt="Picture of the author"
-                    />
-                    <h1 className='font-semibold text-xl text-[#1f2937] mb-2'>David Kim</h1>
-                    <p className='text-[#64748b]'>62d ago</p>
-                    <div className="badge badge-info font-semibold text-[#244d3f] bg-[#cbfadb] mt-2">WORK</div>
-                    <div className="badge badge-warning text-white bg-[#efad44] rounded-full mt-2">Warning</div>
-                </div>
+                <div className={`badge badge-warning text-white ${friend.status === "almost due" ? "bg-[#efad44]" : " "} ${friend.status === "overdue" ? "bg-[#ef4444]" : " "} ${friend.status === "on-track" ? "bg-[#244d3f]" : " "}  rounded-full mt-2`}>{friend.status}</div>
             </div>
         </div>
     );
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,43 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Banner from "./components/Banner/Banner";
 import Card from "./components/Card/Card";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [friends,setFriends]=useState([]);
-  useEffect(()=>{
+  const [friends, setFriends] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
     fetch("/friends.json")
-    .then((res)=>res.json())
-    .then((data)=>{
-      setFriends(data);
-      console.log(data);
-    })
-  },[])
+      .then((res) => res.json())
+      .then((data) => {
+        setFriends(data);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
       <div className="bg-[#f8fafc] py-20">
         <div>
           <Banner></Banner>
-          <Card></Card>
+          <div className="container mx-auto p-4 lg:p-0">
+            <h1 className="font-bold text-[#1f2937] text-2xl">Your Friends</h1>
+            <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+              {loading ? (
+                <div className="flex col-span-full justify-center items-center">
+                  <span className="loading loading-bars loading-xs"></span>
+                  <span className="loading loading-bars loading-sm"></span>
+                  <span className="loading loading-bars loading-md"></span>
+                  <span className="loading loading-bars loading-lg"></span>
+                  <span className="loading loading-bars loading-xl"></span>
+                </div>
+              ) : (
+                friends.map((friend) => (
+                  <Card key={friend.id} friend={friend}></Card>
+                ))
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
